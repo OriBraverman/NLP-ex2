234e6aebc522a71f3166bce440ccedfa8caded44
OriBraverman <oribraverman@gmail.com>
2024-02-03 20:19:22 +0200

    minur change

--- part 1/part 1.2.py
+++ part 1/part 1.2.py
@@ -2,13 +2,15 @@ import os
 import gensim.downloader as dl
 from collections import Counter
 from sklearn.neighbors import KNeighborsClassifier
+from dim_reduction_utils import reduce_dim_fit, reduce_dim_infer
 
 #  Constants
-model_google = dl.load("word2vec-google-news-300")
+model = dl.load("word2vec-google-news-300")
 TRAIN_DATA_PATH = "data/ass1-tagger-train"
 TEST_DATA_PATH = "data/ass1-tagger-dev-input"
 OUTPUT_PATH = "output/part1.2-dev-output.txt"
 LABELED_TEST_DATA_PATH = "data/ass1-tagger-dev"
+
 
 def create_output_file(output_path):
     if not os.path.exists("output"):
@@ -21,37 +23,42 @@ def read_train_data(file_path):
         data = f.read().split()
     return [tuple(x.split("/")) for x in data]
 
-def train_tagger(train_data):
+def train_knn_tagger(train_data):
     """use the method of K-nearest neighbors to tag the words in the train data"""
     X = []
     y = []
     for item in train_data:
         if len(item) == 2:
             word, tag = item
-            if word in model_google:
-                X.append(model_google[word])
+            if word in model:
+                X.append(model[word])
                 y.append(tag)
-    knn = KNeighborsClassifier(n_neighbors=1)
+    pca = reduce_dim_fit(X)
+    X = reduce_dim_infer(pca, X)
+    knn = KNeighborsClassifier(n_neighbors=10)
     knn.fit(X, y)
-    return knn
+    return knn, pca
 
-def tag_test_data(test_data, word_tag):
+def train_freq_tagger(train_data):
+    """get for each word the most common tag"""
+    word_tag = {}
+    for item in train_data:
+        if len(item) == 2:
+            word, tag = item
+            word_tag.setdefault(word, Counter())[tag] += 1
+    return {word: tags.most_common(1)[0][0] for word, tags in word_tag.items()}
+
+def tag_test_data(test_data, freq_tag, knn_tag, pca):
     """map each word in the test data to tuple (word, tag) based on word_tag dictionary"""
     tagged_data = []
     for word in test_data:
-        if word in word_tag:
-            tagged_data.append((word, word_tag[word]))
+        if word in model:
+            tag = knn_tag.predict(reduce_dim_infer(pca, [model[word]]))[0]
+        elif word in freq_tag:
+            tag = freq_tag[word]
         else:
-            # if the word is not in the train data, check the 10 most similar words to it in the model
-            if word in model_google:
-                most_similar = model_google.most_similar(word, topn=10)
-                for similar_word, _ in most_similar:
-                    if similar_word in word_tag:
-                        tagged_data.append((word, word_tag[similar_word]))
-                        break
-            else:
-                # if no similar word is in the train data, tag the word as NN
-                tagged_data.append((word, "NN"))
+            tag = "NN"
+        tagged_data.append((word, tag))
     return tagged_data
 
 def check_accuracy():
@@ -66,17 +73,22 @@ def check_accuracy():
                     same += 1
                 total += 1
     print(f"Accuracy: {same/total}")
+
 def main():
     train_data = read_train_data(TRAIN_DATA_PATH)
 
-    word_tag = train_tagger(train_data)
+    freq_tag = train_freq_tagger(train_data)
+    knn_tag, pca = train_knn_tagger(train_data)
 
     create_output_file(OUTPUT_PATH)
 
+    count = 0
     with open(TEST_DATA_PATH, "r") as f1, open(OUTPUT_PATH, "w") as f2:
         for line in f1:
+            count += 1
+            print(f"line {count} from 1500")
             test_data = line.split()
-            tagged_test_data = tag_test_data(test_data, word_tag)
+            tagged_test_data = tag_test_data(test_data, freq_tag, knn_tag, pca)
             # write the output to file in a format w1/t1 w2/t2 ...
             f2.write(" ".join(["{}/{}".format(word, tag) for word, tag in tagged_test_data]))
             f2.write("\n")
