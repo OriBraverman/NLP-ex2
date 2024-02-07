b611d0c96b341c3afab51af8567decddce01b9e8
YanivZimmer <zimmer.yaniv@gmail.com>
2024-02-07 16:46:16 +0200

    clf test data for ner

--- part 2/ner_utils.py
+++ part 2/ner_utils.py
@@ -14,9 +14,11 @@ def read_data(fname):
 def train_knn(train_data,model):
     data = []
     labels = []
-    for item in train_data:
-        if len(item) == 2:
-            word, tag = item
+    for line in train_data:
+        for pair in line:
+            if len(pair) != 2:
+                continue
+            word, tag = pair
             if word in model:
                 data.append(model[word])
                 labels.append(tag)
@@ -25,12 +27,23 @@ def train_knn(train_data,model):
     knn = KNeighborsClassifier(n_neighbors=10)
     knn.fit(X, labels)
     return knn, pca
-
+def tag_test_data(test_data,emb_model, clf, pca):
+    tagged_data = []
+    for line in test_data:
+        for word in line:
+            if word[0] in emb_model:
+                tag = clf.predict(reduce_dim_infer(pca, [emb_model[word[0]]]))[0]
+            else:
+                tag = "O"
+            tagged_data.append((word, tag))
+    return tagged_data
 def main():
     google_model = dl.load("word2vec-google-news-300")
-    train_path = 'part 2/ner/train'
+    train_path = './ner/train'
+    test_path = './ner/test.blind'
     tagged_data = read_data(train_path)
     clf, dim_red = train_knn(tagged_data,model=google_model)
-
+    test=tag_test_data(read_data(test_path),emb_model=google_model,clf=clf,pca=dim_red)
+    print(test)
 if __name__=='__main__':
     main()
