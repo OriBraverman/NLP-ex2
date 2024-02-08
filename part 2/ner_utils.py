cb6439f5cd5f37405e10f6fb221f7ad0fa5f6b27
YanivZimmer <zimmer.yaniv@gmail.com>
2024-02-08 20:01:12 +0200

    fix evaluation, better print output to file, and use validation split

--- part 2/ner_utils.py
+++ part 2/ner_utils.py
@@ -11,7 +11,7 @@ def read_data(fname):
         line = line.strip().split()
         tagged = [x.rsplit("/",1) for x in line]
         yield tagged
-def train_knn(train_data,model):
+def build_k_nearest(train_data, model):
     data = []
     labels = []
     for line in train_data:
@@ -30,7 +30,6 @@ def train_knn(train_data,model):
 def tag_test_data(test_data,emb_model, clf, pca,output_file):
     tagged_data = []
     with open(output_file,'w') as f:
-        tagged_lines=[]
         for line in test_data:
             tagged_line = ""
             for word in line:
@@ -38,19 +37,23 @@ def tag_test_data(test_data,emb_model, clf, pca,output_file):
                     tag = clf.predict(reduce_dim_infer(pca, [emb_model[word[0]]]))[0]
                 else:
                     tag = "O"
-                tagged_line+=f'{word}/{tag}'
+                tagged_line+=f'{word[0]}/{tag} '
                 tagged_data.append((word[0], tag))
-                tagged_lines.append(tagged_line)
-        f.writelines(tagged_lines)
+                #tagged_lines.append(tagged_line)
+            f.writelines([tagged_line,"\n"])
     return tagged_data
 def main():
     google_model = dl.load("word2vec-google-news-300")
     train_path = './ner/train'
+    val_path = './ner/val'
     test_path = './ner/test.blind'
     tagged_data = read_data(train_path)
-    clf, dim_red = train_knn(tagged_data,model=google_model)
-    test=tag_test_data(read_data(test_path),emb_model=google_model,clf=clf,pca=dim_red,output_file='predicted_file')
-    train=tag_test_data(read_data(train_path),emb_model=google_model,clf=clf,pca=dim_red,output_file='train_pred')
+    print("training")
+    clf, dim_red = build_k_nearest(tagged_data, model=google_model)
+    print("done training")
+    val=tag_test_data(read_data(val_path),emb_model=google_model,clf=clf,pca=dim_red,output_file='val_pred')
+    print(val)
+    test=tag_test_data(read_data(test_path), emb_model=google_model, clf=clf, pca=dim_red, output_file='predicted_file')
     print(test)
 if __name__=='__main__':
     main()
